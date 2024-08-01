7d96b5f8b86ef751d83a1f2e453de3b0f9a62a34
Chandler Abril <abrilchandler1@gmail.com>
2024-07-31 17:14:15 -0700

    Almost complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0, total=0, items=[], quantity=1, last_transaction=0):
+        self.discount = discount
+        self.total = total
+        self.items = items
+        self.quantity = quantity
+        self.last_transaction = last_transaction
+
+    def add_item(self, title, price, quantity=1):
+        self.item_quantity(title, price, quantity)
+        self.last_transaction = price * quantity
+
+    def item_quantity(self, title, price, quantity):
+        total = price * quantity
+        self.total += total
+        self.items.append(title)
+
+    def apply_discount(self):
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction
+        self.items.pop()
